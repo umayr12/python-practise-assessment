b664a5c27873df34d0d14716c796c0e76bbab58e
umayr12 <umayr9@gmail.com>
2021-06-18 00:50:14 +0000

    last1

--- Python-assessment/questions/python1.py
+++ Python-assessment/questions/python1.py
@@ -158,7 +158,10 @@ def four(input):
 	# You may need to create a list of numbers from 0 to i, take a look at help(range).
 
 def five(input):
-	return 1
+	total = 1
+    for num in range(1,input+1):
+		total = total*num
+		return total 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
